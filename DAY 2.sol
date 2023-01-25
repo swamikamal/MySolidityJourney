acaa0260d155f779ce06d021e8e54f896b76fbd6
Kamal Swami <49489982+swamikamal@users.noreply.github.com>
2023-01-25 09:11:02 +0530

    Create DAY 2.sol

---
+++ DAY 2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Day2{
+    uint256 public stateVariable = 10;
+    
+    function returnStateVariable() public view returns(uint256){
+        return stateVariable;
+    }
+
+    function returnLocalVariable() public pure returns(uint256){
+        uint256 localVariable = 20;
+        return localVariable;
+    }
+}
